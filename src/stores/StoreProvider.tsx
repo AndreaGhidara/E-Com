b858121c295e2cd8e9d6b7a0f2fca526174d8558
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2024-11-12 19:07:15 +0100

    feat: add store

--- src/stores/StoreProvider.tsx
+++ src/stores/StoreProvider.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { Provider } from "react-redux"
-import { RootState, store } from "./store"
+import { store } from "./store"
 
-export default function StoreProvider({ children }: { children: RootState }) {
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
     return (
         <Provider store={store}>
             {children}
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '@/stores/slice/counter/counterSlice'
+
 
 
 export const store = configureStore({
     reducer: {
-    
+        counter: counterReducer
     },
 })
 
+// Get the type of our store variable
+export type AppStore = typeof store
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<AppStore['getState']>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = AppStore['dispatch']
